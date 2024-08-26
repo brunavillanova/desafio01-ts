730e3be69b5466a00c492064b5845fa17e599d81
bruna villanova <brunnavillanova@gmail.com>
2024-08-26 09:42:35 -0300

    atualizado

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from './class/CompanyAccount';
+import { PeopleAccount } from './class/PeopleAccount';
+import { SpecialAccount } from "./class/SpecialAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount = new PeopleAccount(1, 'Bruna', 100);
+console.log('PeopleAccount:', peopleAccount);
+peopleAccount.deposit(100);
+console.log('PeopleAccount após depósito:', peopleAccount);
+
+const companyAccount = new CompanyAccount('DIO', 110);
+companyAccount.getLoan(10);  // Usa o método público getLoan para adicionar ao saldo
+console.log('CompanyAccount após empréstimo:', companyAccount);
+
+const specialAccount = new SpecialAccount('Special', 200);
+specialAccount.deposit(100);  // Deposita com bônus de 10
+console.log('SpecialAccount após depósito:', specialAccount);
